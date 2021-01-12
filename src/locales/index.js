9be3fd4dac282af1897ef1e71f62ac39d752bb35
mthang1801 <mthang1801@gmail.com>
2021-01-12 22:39:56 +0700

    complete demo

---
+++ src/locales/index.js
@@ -0,0 +1,6 @@
+import {vi} from "./vi/translation"
+import {en} from "./en/translation"
+export default {
+  vi,
+  en
+}
---
+++ src/locales/vi/translation.js
@@ -0,0 +1,9 @@
+export const vi = {
+  translation: {
+    title: "Chào mừng đến với react-i18next",
+    description: {
+      part1: "Để bắt đầu, thay đổi<1>src/App.js</1> and save to reload.",
+      part2: "Đổi ngôn ngữ giữa tiếng Anh và tiếng Việt bằng nút phía trên",
+    },
+  },
+};
